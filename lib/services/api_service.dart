cad78b38422e895f52e84f1e5f7cee7d20391c4a
naimulhassan2001 <developernaimul00@gmail.com>
2025-02-01 18:37:05 +0600

    improve code

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,227 +1,130 @@
 import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
+import 'package:api_service_with_dio/helpers/prefs_helper.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
 
 import '../model/api_response_model.dart';
 import '../utils/app_string.dart';
 
-
 class ApiService {
   static final Dio _dio = getMyDio();
-  static String token = "";
 
   static Future<ApiResponseModel> postApi(String url, body,
       {Map<String, String>? header}) async {
-    dynamic responseJson;
-
-    Options options = Options(headers: header);
-
-    try {
-      Response response = await _dio.post(url, data: body, options: options);
-
-      responseJson = handleResponse(response);
-    } on SocketException {
-      return ApiResponseModel(503, AppString.noInternetConnection, {});
-    } on FormatException {
-      return ApiResponseModel(400, AppString.badResponseRequest, {});
-    } on TimeoutException {
-      return ApiResponseModel(408, AppString.requestTimeOut, {});
-    } catch (e) {
-      return ApiResponseModel(400, e.toString(), {});
-    }
-
-    return responseJson;
+    return requestApi(url, "POST", body: body, header: header);
   }
-
-  ///<<<======================== Get Api ==============================>>>
 
   static Future<ApiResponseModel> getApi(String url,
       {Map<String, String>? header}) async {
-    dynamic responseJson;
-
-    Options options = Options(headers: header);
-
-    try {
-      Response response = await _dio.get(url, options: options);
-
-      responseJson = handleResponse(response);
-    } on SocketException {
-      return ApiResponseModel(503, AppString.noInternetConnection, {});
-    } on FormatException {
-      return ApiResponseModel(400, AppString.badResponseRequest, {});
-    } on TimeoutException {
-      return ApiResponseModel(408, AppString.requestTimeOut, {});
-    } catch (e) {
-      return ApiResponseModel(400, e.toString(), {});
-    }
-    return responseJson;
+    return requestApi(url, "GET", header: header);
   }
 
-  ///<<<======================== Put Api ==============================>>>
-
-  static Future<ApiResponseModel> putApi(String url, Map<String, String> body,
+  static Future<ApiResponseModel> putApi(String url, body,
       {Map<String, String>? header}) async {
-    dynamic responseJson;
-
-    Options options = Options(headers: header);
-
-    try {
-      Response response = await _dio.post(url, data: body, options: options);
-
-      responseJson = handleResponse(response);
-    } on SocketException {
-      return ApiResponseModel(503, AppString.noInternetConnection, {});
-    } on FormatException {
-      return ApiResponseModel(400, AppString.badResponseRequest, {});
-    } on TimeoutException {
-      return ApiResponseModel(408, AppString.requestTimeOut, {});
-    } catch (e) {
-      return ApiResponseModel(400, e.toString(), {});
-    }
-    return responseJson;
+    return requestApi(url, "PUT", body: body, header: header);
   }
 
-  ///<<<======================== Patch Api ==============================>>>
-
-  static Future<ApiResponseModel> patchApi(
-    String url, {
-    Map<String, String>? body,
-    Map<String, String>? header,
-  }) async {
-    dynamic responseJson;
-    Options options = Options(headers: header);
-
-    try {
-      if (body != null) {
-        Response response = await _dio.post(url, data: body, options: options);
-        responseJson = handleResponse(response);
-      } else {
-        Response response = await _dio.post(url, options: options);
-
-        responseJson = handleResponse(response);
-      }
-    } on SocketException {
-      return ApiResponseModel(503, AppString.noInternetConnection, {});
-    } on FormatException {
-      return ApiResponseModel(400, AppString.badResponseRequest, {});
-    } on TimeoutException {
-      return ApiResponseModel(408, AppString.requestTimeOut, {});
-    } catch (e) {
-      return ApiResponseModel(400, e.toString(), {});
-    }
-
-    return responseJson;
+  static Future<ApiResponseModel> patchApi(String url, body,
+      {Map<String, String>? header}) async {
+    return requestApi(url, "PATCH", body: body, header: header);
   }
-
-  ///<<<======================== Delete Api ==============================>>>
 
   static Future<ApiResponseModel> deleteApi(String url,
       {Map<String, String>? body, Map<String, String>? header}) async {
-    dynamic responseJson;
-
-    Options options = Options(headers: header);
-
-    try {
-      if (body != null) {
-        Response response = await _dio.post(url, data: body, options: options);
-
-        responseJson = handleResponse(response);
-      } else {
-        Response response = await _dio.post(url, options: options);
-        responseJson = handleResponse(response);
-      }
-    } on SocketException {
-      return ApiResponseModel(503, AppString.noInternetConnection, {});
-    } on FormatException {
-      return ApiResponseModel(400, AppString.badResponseRequest, {});
-    } on TimeoutException {
-      return ApiResponseModel(408, AppString.requestTimeOut, {});
-    } catch (e) {
-      return ApiResponseModel(400, e.toString(), {});
-    }
-    return responseJson;
+    return requestApi(url, "DELETE", body: body, header: header);
   }
 
-  static dynamic handleResponse(Response response) {
-    switch (response.statusCode) {
-      case 200:
-        return ApiResponseModel(
-            response.statusCode, response.data['message'], response.data);
-      case 201:
-        return ApiResponseModel(200, response.data['message'], response.data);
-      case 401:
-        // Get.offAllNamed(AppRoutes.signInScreen);
-        return ApiResponseModel(
-            response.statusCode, response.data['message'], response.data);
-      case 400:
-        return ApiResponseModel(
-            response.statusCode, response.data['message'], response.data);
-      case 404:
-        return ApiResponseModel(
-            response.statusCode, response.data['message'], response.data);
-      default:
-        return ApiResponseModel(
-            response.statusCode, response.data['message'], response.data);
+
+
+  static Future<ApiResponseModel> requestApi(
+    String url,
+    String method, {
+    dynamic body,
+    Map<String, String>? header,
+  }) async {
+    try {
+      Response response = await _dio.request(
+        url,
+        data: body,
+        options: Options(method: method, headers: header),
+      );
+      return handleResponse(response);
+    } catch (e) {
+      return handleError(e);
+    }
+  }
+
+  static ApiResponseModel handleResponse(Response response) {
+    return ApiResponseModel(response.statusCode ?? 500,
+        response.data['message'] ?? "", response.data);
+  }
+
+  static ApiResponseModel handleError(dynamic error) {
+    if (error is DioException) {
+      switch (error.type) {
+        case DioExceptionType.connectionTimeout:
+        case DioExceptionType.receiveTimeout:
+        case DioExceptionType.sendTimeout:
+          return ApiResponseModel(408, AppString.requestTimeOut, {});
+        case DioExceptionType.connectionError:
+          return ApiResponseModel(503, AppString.noInternetConnection, {});
+        default:
+          return ApiResponseModel(
+              error.response?.statusCode ?? 500, error.message ?? "", {});
+      }
+    } else if (error is SocketException) {
+      return ApiResponseModel(503, AppString.noInternetConnection, {});
+    } else if (error is FormatException) {
+      return ApiResponseModel(400, AppString.badResponseRequest, {});
+    } else if (error is TimeoutException) {
+      return ApiResponseModel(408, AppString.requestTimeOut, {});
+    } else {
+      return ApiResponseModel(400, error.toString(), {});
     }
   }
 }
 
 Dio getMyDio() {
   Dio dio = Dio();
-
   Stopwatch stopwatch = Stopwatch();
-
   dio.interceptors.add(InterceptorsWrapper(
     onRequest: (options, handler) {
-      options.headers["Authorization"] = ApiService.token;
+      options.headers["Authorization"] = PrefsHelper.token;
       options.headers["Content-Type"] = "application/json";
-      options.sendTimeout = Duration(seconds: 30);
-      options.receiveTimeout = Duration(seconds: 30);
-      options.receiveDataWhenStatusError = true;
+      options.sendTimeout = const Duration(seconds: 30);
+      options.receiveTimeout = const Duration(seconds: 30);
 
       if (kDebugMode) {
         stopwatch.start();
-        print("Requested URL: ${options.uri}");
-        print("Request Headers: ${options.headers}");
-        print("Request Body: ${jsonEncode((options.data))}");
+        print("Api Service ==================>Requested method: ${options.method}");
+        print("Api Service==================>Requested URL: ${options.uri}");
+        print("Api Service==================>Request Headers: ${options.headers}");
+        print("Api Service==================>Request Body: ${jsonEncode(options.data)}");
       }
-
       handler.next(options);
     },
     onResponse: (response, handler) {
       if (kDebugMode) {
         stopwatch.stop();
-        print(
-            "Response Time: ${stopwatch.elapsedMilliseconds / 1000} second\n");
-
-        print("Response Status Code: ${response.statusCode}");
-        print("Response Data: ${jsonEncode(response.data)}");
-
+        print("Api Service==================>Response Time: ${stopwatch.elapsedMilliseconds / 1000} Second");
+        print("Api Service==================>Response Status Code: ${response.statusCode}");
+        print("Api Service==================>Response Data: ${jsonEncode(response.data)}");
         stopwatch.reset();
       }
-
       handler.next(response);
     },
     onError: (error, handler) {
       if (kDebugMode) {
         stopwatch.stop();
-
-        print(
-            "Response Time: ${stopwatch.elapsedMilliseconds / 1000} second\n");
-
-        print("Error Status Code: ${error.response?.statusCode}");
-        print("Error Data: ${jsonEncode(error.response?.data)}");
+        print("Api Service==================>Response Time: ${stopwatch.elapsedMilliseconds / 1000} Second");
+        print("Api Service==================>Error Status Code: ${error.response?.statusCode}");
+        print("Api Service==================>Error Data: ${jsonEncode(error.response?.data)}");
         stopwatch.reset();
       }
-      if (error.type == DioExceptionType.connectionError) {
-        print("Error fdfdsfdsfdsfds Code: ${error.response?.statusCode}");
-      }
-
       handler.next(error);
     },
   ));
-
   return dio;
 }
